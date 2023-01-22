c66260d0d1b37a2d9dc449b3a7eda8b698c4b43a
emangomaa <emangomaa149@gmail.com>
2023-01-22 19:36:51 +0200

    delete booksshelf and restructure categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,21 +1,54 @@
-import BooksShelf from "./BooksShelf";
+
 import { Link } from "react-router-dom";
-const Categories = ({books})=>{
-  const categories = ['currentlyReading','wantToRead','read']
+import Book from "./Book";
+import PropTypes from "prop-types";
+const Categories = ({books,updateBooks})=>{
     return(
         <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-            {
-              categories.map(category => <BooksShelf key={category} shelfTitle={category} shelfBooks = {books}/>)
-            }
+          <div className="bookshelf">
+              <h2 className="bookshelf-title">currentlyReading</h2>
+              <div className="bookshelf-books">
+                  <ol className="books-grid">
+                  {
+                    books.filter(book => book.shelf === "currentlyReading").map(book =><Book key={book.id} book={book} updateBooks={updateBooks}/> )
+                  }
+                  </ol>
+              </div>
+          </div>
+          <div className="bookshelf">
+              <h2 className="bookshelf-title">wantToRead</h2>
+              <div className="bookshelf-books">
+                  <ol className="books-grid">
+                  {
+                    books.filter(book => book.shelf === "wantToRead").map(book =><Book key={book.id} book={book} updateBooks={updateBooks}/> )
+                  }
+                  </ol>
+              </div>
+          </div>
+          <div className="bookshelf">
+              <h2 className="bookshelf-title">read</h2>
+              <div className="bookshelf-books">
+                  <ol className="books-grid">
+                  {
+                    books.filter(book => book.shelf === "read").map(book =><Book key={book.id} book={book} updateBooks={updateBooks}/> )
+                  }
+                  </ol>
+              </div>
+          </div>
         </div>
         <div className="open-search">
             <Link to='./search' ></Link>
           </div>
       </div>
     )
+}
+
+Categories.propTypes={
+  books:PropTypes.array.isRequired,
+  updateBooks:PropTypes.func.isRequired
 }
 export default Categories;
